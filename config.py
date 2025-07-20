7e58c4f88b8a53e4bcb4cff84e6290d6985d2196
giz73 <nneven@proton.m>
2025-07-20 17:48:27 +0200

    dodatno rad na dodavanju i brisanju

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost/restaurant"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://giz73:naN1ca@localhost/restaurant"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "123tajni456kljuc789"
