73a57c4ea40d54699a8f01bf5a976dd34ef14738
giz73 <nneven@proton.m>
2025-07-22 21:00:50 +0200

    admin forme i table

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ from . import db
 class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
